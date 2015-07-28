ed7b15a08ef54f1c4b7b5c1c1093bd1e3f6b43b6
Lucas Meneghel Rodrigues <lmr@redhat.com>
2015-07-28 00:59:09 -0300

    aexpect.utils.genio: Remove unnecessary variable
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- aexpect/utils/genio.py
+++ aexpect/utils/genio.py
@@ -1,11 +1,10 @@
 import os
 
 _open_log_files = {}
-_log_file_dir = os.environ.get('TMPDIR', '/tmp')
 
 
 def close_log_file(filename):
-    global _open_log_files, _log_file_dir
+    global _open_log_files
     remove = []
     for k in _open_log_files:
         if os.path.basename(k) == filename:
