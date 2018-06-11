45f502579175a1b3ef3dce549168df8d71f41253
Cleber Rosa <crosa@redhat.com>
2018-06-11 08:56:26 -0400

    setup.py: remove unused code
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- setup.py
+++ setup.py
@@ -13,41 +13,10 @@
 # Copyright: Red Hat Inc. 2013-2015
 # Author: Lucas Meneghel Rodrigues <lmr@redhat.com>
 
-import os
 import sys
 # pylint: disable=E0611
 
 from setuptools import setup
-
-VIRTUAL_ENV = 'VIRTUAL_ENV' in os.environ
-
-
-def get_dir(system_path=None, virtual_path=None):
-    """
-    Retrieve VIRTUAL_ENV friendly path
-    :param system_path: Relative system path
-    :param virtual_path: Overrides system_path for virtual_env only
-    :return: VIRTUAL_ENV friendly path
-    """
-    if virtual_path is None:
-        virtual_path = system_path
-    if VIRTUAL_ENV:
-        if virtual_path is None:
-            virtual_path = []
-        return os.path.join(*virtual_path)
-    else:
-        if system_path is None:
-            system_path = []
-        return os.path.join(*(['/'] + system_path))
-
-
-def get_avocado_libexec_dir():
-    if VIRTUAL_ENV:
-        return get_dir(['libexec'])
-    elif os.path.exists('/usr/libexec'):    # RHEL-like distro
-        return get_dir(['usr', 'libexec', 'avocado'])
-    else:                                   # Debian-like distro
-        return get_dir(['usr', 'lib', 'avocado'])
 
 
 if __name__ == '__main__':
