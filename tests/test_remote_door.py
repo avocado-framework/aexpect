d208b427251605ac8ee815d3a990597dc1684c07
Plamen Dimitrov <pdimitrov@pevogam.com>
2020-04-06 22:29:14 +0300

    Update all unit tests with mock objects and extended coverage

--- tests/test_remote_door.py
+++ tests/test_remote_door.py
@@ -14,41 +14,50 @@
 #
 # selftests pylint: disable=C0111,C0111
 
+import os
+import glob
+import re
 import unittest
+import unittest.mock as mock
 
-import remote_door
-
-
-class DummySession(object):  # pylint: disable=R0903
-    """Dummy class for a session."""
-
-    def __init__(self):
-        self.recorded_calls = []
-
-    def cmd(self, command, print_func=None):  # pylint: disable=W0613
-        """Dummy cmd call."""
-        self.recorded_calls.append(command)
+from aexpect import remote_door
 
 
+@remote_door.run_remotely
+def add_one(number):
+    """A small decorated test function."""
+    return number + 1
+
+
+@mock.patch('aexpect.remote_door.remote', mock.MagicMock())
 class RemoteDoorTest(unittest.TestCase):
     """Unit test class for the remote door."""
 
     def setUp(self):
-        self.session = DummySession()
+        self.session = mock.MagicMock(name='session')
+
+    def tearDown(self):
+        for control_file in glob.glob("tmp*.control"):
+            os.unlink(control_file)
 
     def test_run_remote_util(self):
         """Test that a remote utility can be run properly."""
         remote_door.run_remote_util(self.session, "foo_util", "bar_func",
                                     42, "spring", sing="zazz")
-        self.assertIn("> /tmp/control", self.session.recorded_calls[0],
-                      "A control file has to be generated for the peer")
-        self.assertIn("import foo_util", self.session.recorded_calls[0])
-        self.assertIn("foo_util.bar_func(42, \'\"\'\"\'spring\'\"\'\"\', "
-                      "sing=\'\"\'\"\'zazz\'\"\'\"\')",
-                      self.session.recorded_calls[0])
-        self.assertEqual(self.session.recorded_calls[1],
-                         "python3 /tmp/control",
-                         "The peer has to be called to run the generated control")
+        # python 3.6 and above
+        if hasattr(self.session.cmd, "assert_called_once"):
+            self.session.cmd.assert_called_once()
+        else:
+            self.assertEqual(self.session.cmd.call_count, 1)
+        command = self.session.cmd.call_args[0][0]
+        self.assertTrue(re.match(r"python3 /tmp/tmp.+\.control", command),
+                        "A control file has to be generated and called on the peer")
+        control = os.path.basename(command.lstrip("python3 "))
+        with open(control) as handle:
+            control_lines = handle.readlines()
+            self.assertIn("import foo_util\n", control_lines)
+            self.assertIn("result = foo_util.bar_func(42, r'spring', sing=r'zazz')\n",
+                          control_lines)
 
     def test_run_remote_util_object(self):
         """Test that a remote utility object can be run properly."""
@@ -56,12 +65,59 @@ class RemoteDoorTest(unittest.TestCase):
         remote_door.run_remote_util(self.session, "foo_util",
                                     "%s.baz_func" % util_object,
                                     "Wonderland is fun", wanderer=None)
-        self.assertIn("> /tmp/control", self.session.recorded_calls[0],
-                      "A control file has to be generated for the peer")
-        self.assertIn("import foo_util", self.session.recorded_calls[0])
-        self.assertIn("foo_util.BarClass(val1, \'\"\'\"\'val2\'\"\'\"\').baz_func("
-                      "\'\"\'\"\'Wonderland is fun\'\"\'\"\', wanderer=None)",
-                      self.session.recorded_calls[0])
-        self.assertEqual(self.session.recorded_calls[1],
-                         "python3 /tmp/control",
-                         "The peer has to be called to run the generated control")
+        # python 3.6 and above
+        if hasattr(self.session.cmd, "assert_called_once"):
+            self.session.cmd.assert_called_once()
+        else:
+            self.assertEqual(self.session.cmd.call_count, 1)
+        command = self.session.cmd.call_args[0][0]
+        self.assertTrue(re.match(r"python3 /tmp/tmp.+\.control", command),
+                        "A control file has to be generated and called on the peer")
+        control = os.path.basename(command.lstrip("python3 "))
+        with open(control) as handle:
+            control_lines = handle.readlines()
+            self.assertIn("import foo_util\n", control_lines)
+            self.assertIn("result = foo_util.BarClass(val1, 'val2').baz_func("
+                          "r'Wonderland is fun', wanderer=None)\n",
+                          control_lines)
+
+    def test_run_remote_decorator(self):
+        """Test that a remote utility object can be run properly."""
+        _ = add_one(self.session, 3)  # pylint: disable=E1121
+        # python 3.6 and above
+        if hasattr(self.session.cmd, "assert_called_once"):
+            self.session.cmd.assert_called_once()
+        else:
+            self.assertEqual(self.session.cmd.call_count, 1)
+        command = self.session.cmd.call_args[0][0]
+        self.assertTrue(re.match(r"python3 /tmp/tmp.+\.control", command),
+                        "A control file has to be generated and called on the peer")
+        control = os.path.basename(command.lstrip("python3 "))
+        with open(control) as handle:
+            control_lines = handle.readlines()
+            self.assertIn("def add_one(number):\n", control_lines)
+            self.assertIn("    return number + 1\n", control_lines)
+            self.assertIn("result = add_one(3)\n", control_lines)
+
+    def test_run_remote_object(self):
+        """Test that a remote utility object can be run properly."""
+        remote_door.Pyro4 = mock.MagicMock()
+        disconnect = remote_door.Pyro4.errors.PyroError = Exception
+        remote_door.Pyro4.Proxy.side_effect = [disconnect("no such object"), mock.DEFAULT]
+        self.session.get_output.return_value = "ready"
+        remote_door.get_remote_object("module.MyClass", self.session,
+                                      "testhost", 4242)
+        if hasattr(self.session.cmd, "assert_called_once"):
+            self.session.sendline.assert_called_once()
+        else:
+            self.assertEqual(self.session.sendline.call_count, 1)
+        command = self.session.sendline.call_args[0][0]
+        self.assertTrue(re.match(r"python3 /tmp/tmp.+\.control", command),
+                        "A control file has to be generated and called on the peer")
+        control = os.path.basename(command.lstrip("python3 "))
+        with open(control) as handle:
+            control_lines = handle.readlines()
+            self.assertIn("import remote_door\n", control_lines)
+            self.assertIn("result = remote_door.share_local_object(r'module.MyClass', "
+                          "host=r'testhost', port=4242)\n",
+                          control_lines)
